fc7807ad64992b76248c0308d38d929dcec374c7
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-02-02 03:35:43 -0300

    adicionando perfil dev

--- src/main/java/com/tiago/ordemservico/OrdemServicoApplication.java
+++ src/main/java/com/tiago/ordemservico/OrdemServicoApplication.java
@@ -1,19 +1,13 @@
 package com.tiago.ordemservico;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrdemServicoApplication implements CommandLineRunner {
+public class OrdemServicoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrdemServicoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 	}
 
 }
